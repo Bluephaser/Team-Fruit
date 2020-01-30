ceab5a488173e8343f4685253eec3c5b0913d68a
Bluephaser <liam.binford@digipen.edu>
2020-01-30 13:46:15 -0800

    ikd trying to push again

---
+++ Frogger/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * PlayerController.cs
+ * By: Liam Binford
+ * Date: 1/30/20
+ * Description: Script for making the player character move
+ * **/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool canMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
