a7bab0e4d1449c88150f255292f38669e3163f46
Bluephaser <liam.binford@digipen.edu>
2020-01-30 14:59:06 -0800

    Player Controller

--- Frogger/Assets/Scripts/PlayerController.cs
+++ Frogger/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     bool canMove = true;
+    Vector2 Destination = new Vector2();
+    Rigidbody2D myRB;
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,13 +26,45 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetAxis("Vertical") > 0.01f && canMove)
         {
+            //disable movement until the player has moved an entire square
             canMove = false;
+            Destination = new Vector2(0, transform.position.y + 1);
 
+            StartCoroutine(Move());
+        }
+        if (Input.GetAxis("Vertical") < -0.01f && canMove)
+        {
+            //disable movement until the player has moved an entire square
+            canMove = false;
+            Destination = new Vector2(0, transform.position.y -1);
+
+            StartCoroutine(Move());
+        }
+        if (Input.GetAxis("Horizontal") > 0.01f && canMove)
+        {
+            //disable movement until the player has moved an entire square
+            canMove = false;
+            Destination = new Vector2(transform.position.x + 1, 0);
+
+            StartCoroutine(Move());
+        }
+        if (Input.GetAxis("Horizontal") < -0.01f && canMove)
+        {
+            //disable movement until the player has moved an entire square
+            canMove = false;
+            Destination = new Vector2(transform.position.x - 1, 0);
+
+            StartCoroutine(Move());
         }
     }
 
     IEnumerator Move()
     {
-        return;
+        while(Vector2.Distance(transform.position, Destination) > 0.001f)
+        {
+            myRB.MovePosition(Vector2.MoveTowards(transform.position, Destination, speed));
+            yield return new WaitForFixedUpdate();
+        }
+        canMove = true;
     }
 }
