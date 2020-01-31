d88ebd063ab5420e3d08758fde388831f00877c5
CJ <charles.sweatt>
2020-01-31 14:30:55 -0800

    added scripts for cars

--- Frogger/Assets/Scripts/CarController.cs
+++ Frogger/Assets/Scripts/CarController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int dir;
+    public float speed;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        var tranpos = transform.position;
+        GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(tranpos, tranpos + new Vector3(dir, 0, 0), speed));
     }
 }
---
+++ Frogger/Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Vector3 dest;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(gameObject.GetComponent<CarController>() != null)
+            //if CarController exists within collided object
+        {
+            gameObject.GetComponent<CarController>().transform.position = new Vector2();
+                //this is asking for gameObjects that have CarController
+                //therefore this refers to the whole gameObject, not the car controller
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
