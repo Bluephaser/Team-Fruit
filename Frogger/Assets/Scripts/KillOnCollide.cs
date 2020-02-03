b5fa64f19c5f3970029f0b66884f9e6a52d06b37
Bluephaser <liam.binford@digipen.edu>
2020-02-03 14:46:41 -0800

    changed sensitivity

---
+++ Frogger/Assets/Scripts/KillOnCollide.cs
@@ -0,0 +1,29 @@
+﻿/* KillOnCollide.cs
+ * By: Liam Binford 
+ * Date: 2/3/20
+ * Description: Kills the player if the object with this script collides with them
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnCollide : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
