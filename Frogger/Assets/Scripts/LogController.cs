5771bd7da4b2b5c3eb1dfe199feccfeec5c4e569
CJ <charles.sweatt>
2020-02-04 13:29:30 -0800

    Yume wo shibaritsukeru
    Omotai sadame no ishi ni
    
    Marude oshitsubusarete
    
    Doko ni mo nigerarenai
    
    Hitasura agakinagara
    
    Dakedo akiramenai
    
    Gareki no soko ni uzumoreta kibou no kakeratachi
    
    Kitto kono te de mitsukedasu sa
    
    Sekai wo kaeru tame ni
    
    Fighting Gold
    
    Jibun no uchi wo shinjita mono dake ga
    
    Itsuka kagayakeru inochi no kagiri
    
    Kami ni mo sakarai tatakai tsuzukeru
    
    Fukutsu no tamashiitachi yo
    
    Fighting Gold

---
+++ Frogger/Assets/Scripts/LogController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogController : MonoBehaviour
+{
+    public int dir;
+    public float speed;
+    public float trigger;
+    public Vector3 dest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 tranpos = transform.position;
+        //this equals that, but NOT VICE VERSA
+        //KEEP THAT IN MIND, DUMB DUMB
+
+        GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(tranpos, tranpos + new Vector3(dir, 0, 0), speed));
+
+        //if direction is left
+        if (dir == -1)
+        {
+            //if position is far enough
+            if (tranpos.x <= trigger)
+            {
+                transform.position = dest;
+            }
+        }
+
+        if (dir == 1)
+        {
+            if (tranpos.x >= trigger)
+            {
+                transform.position = dest;
+            }
+        }
+    }
+}
+
