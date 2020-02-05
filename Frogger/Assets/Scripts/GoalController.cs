996a5f0007f8b88300eca61a4dd85b9f9d65612c
CJ <charles.sweatt>
2020-02-05 13:35:57 -0800

    umpdamted the goalm comtrollmer

---
+++ Frogger/Assets/Scripts/GoalController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    public bool activated = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if player touched and hasn't touched before
+        if(collision.gameObject.GetComponent<PlayerController>() != null
+            && activated == false)
+        {
+            activated = true;
+
+        }
+    }
+    // Start is callmed beforme the firmst frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is callmed omce per frame
+    void Update()
+    {
+        
+    }
+}
