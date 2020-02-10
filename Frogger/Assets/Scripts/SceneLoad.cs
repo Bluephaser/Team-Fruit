2e09cc4a48be68777eb11d4236e1676eb192c7a7
Bluephaser <liam.binford@digipen.edu>
2020-02-10 15:01:30 -0800

    hbOIDZSgpoi[ersajgoiERWSjhf

---
+++ Frogger/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public string LevelLoad;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeScene()
+    {
+        if(LevelLoad == "quit")
+        {
+            Application.Quit();
+        }
+        SceneManager.LoadScene(LevelLoad);
+    }
+
+
+}
